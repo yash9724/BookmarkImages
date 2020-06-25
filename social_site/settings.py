f765e56b8aab2bd7d6e1fd3f18247da700533aa8
yash9724 <yashgupta9724@gmail.com>
2020-06-25 11:00:22 +0530

    user registration, custom authentication and social authentication using facebook, google and twitter added.

--- social_site/settings.py
+++ social_site/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR =  os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,11 @@ SECRET_KEY = '(y%%4h3ua(-=12$(691y%)@ubf0bv$++&41t0myaxb!8zpebqu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'connectto.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -38,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
+    
 ]
 
 MIDDLEWARE = [
@@ -119,3 +126,30 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.twitter.TwitterOAuth',
+    'social_core.backends.google.GoogleOAuth2',
+]
+
+SOCIAL_AUTH_FACEBOOK_KEY = '970403966737203'  # Facebook app ID
+SOCIAL_AUTH_FACEBOOK_SECRET = '0dcfb2cd07f9982ad1e5e6c827518466'
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+
+SOCIAL_AUTH_TWITTER_KEY = 'E0J0BjISAzqMY0ypBKgxGWdSC'
+SOCIAL_AUTH_TWITTER_SECRET = 'FPvzf15loJXMBWBHtjM6Bbks8L0o9J4UjUI8VM6AEHmtRXig55'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '785068402173-l9vbjhdoqp0bn0072vj3kois77bc1upd.apps.googleusercontent.com'
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'cLkOEw3v-aAV4850EkCm23dz'
