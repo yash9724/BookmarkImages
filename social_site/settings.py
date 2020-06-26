59ebe1069317c59651af19c78a379c5db6be2034
yash9724 <yashgupta9724@gmail.com>
2020-06-26 14:58:49 +0530

    image model created and registered with admin site.

--- social_site/settings.py
+++ social_site/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'images.apps.ImagesConfig',
     'social_django',
     'django_extensions',
     
